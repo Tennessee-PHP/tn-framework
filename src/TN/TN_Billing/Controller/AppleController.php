54643d8ff06cbb897856242146cc657618314d2a
Simon Shepherd <89672429+simonrshepherd@users.noreply.github.com>
2025-07-22 20:22:23 -0500

    Add route attributes to AppleController for API subscription endpoint
    - Introduced route attributes including Anyone access restriction, AllowOrigin, and AllowCredentials to the apiSubscription method in AppleController.
    - This change enhances the API's accessibility and security by clearly defining access controls and CORS settings.

--- src/TN/TN_Billing/Controller/AppleController.php
+++ src/TN/TN_Billing/Controller/AppleController.php
@@ -5,11 +5,16 @@ namespace TN\TN_Billing\Controller;
 use TN\TN_Core\Attribute\Route\Path;
 use TN\TN_Core\Attribute\Route\Component;
 use TN\TN_Core\Controller\Controller;
+use TN\TN_Core\Attribute\Route\Access\Restrictions\Anyone;
+use TN\TN_Core\Attribute\Route\AllowOrigin;
+use TN\TN_Core\Attribute\Route\AllowCredentials;
 
 class AppleController extends Controller
 {
     #[Path('api/apple/subscription')]
+    #[Anyone]
+    #[AllowOrigin]
+    #[AllowCredentials]
     #[Component(\TN\TN_Billing\Component\Apple\Api\Subscription::class)]
     public function apiSubscription() {}
-
 }
