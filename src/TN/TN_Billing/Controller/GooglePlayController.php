e610f9f5752083bc5064b0728efe5442c1bd6905
Simon Shepherd <89672429+simonrshepherd@users.noreply.github.com>
2025-07-23 19:22:56 -0500

    Add route attributes to GooglePlayController for API subscription endpoint
    - Introduced route attributes including Anyone access restriction, AllowOrigin, and AllowCredentials to the apiSubscription method in GooglePlayController.
    - This change enhances the API's accessibility and security by clearly defining access controls and CORS settings.

--- src/TN/TN_Billing/Controller/GooglePlayController.php
+++ src/TN/TN_Billing/Controller/GooglePlayController.php
@@ -6,10 +6,16 @@ use TN\TN_Core\Attribute\Route\Path;
 use TN\TN_Core\Attribute\Route\Component;
 use TN\TN_Core\Controller\Controller;
 
+use TN\TN_Core\Attribute\Route\Access\Restrictions\Anyone;
+use TN\TN_Core\Attribute\Route\AllowOrigin;
+use TN\TN_Core\Attribute\Route\AllowCredentials;
+
 class GooglePlayController extends Controller
 {
     #[Path('api/googleplay/subscription')]
+    #[Anyone]
+    #[AllowOrigin]
+    #[AllowCredentials]
     #[Component(\TN\TN_Billing\Component\GooglePlay\Api\Subscription::class)]
     public function apiSubscription() {}
-
 }
