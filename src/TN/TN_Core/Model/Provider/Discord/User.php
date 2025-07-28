03106467580f84d118206a2bc4240df25c777b37
Simon Shepherd <89672429+simonrshepherd@users.noreply.github.com>
2025-07-28 16:08:46 -0500

    Update Discord user redirect URI to utilize BASE_URL for dynamic configuration

--- src/TN/TN_Core/Model/Provider/Discord/User.php
+++ src/TN/TN_Core/Model/Provider/Discord/User.php
@@ -31,7 +31,7 @@ class User extends ThirdPartyUser
         return new DiscordProvider([
             'clientId' => $_ENV['DISCORD_APPLICATION_ID'],
             'clientSecret' => $_ENV['DISCORD_OAUTH_SECRET'],
-            'redirectUri' => 'https://www.footballguys.com/me/profile/connected-accounts/discord'
+            'redirectUri' => $_ENV['BASE_URL'] . 'me/profile/connected-accounts/discord'
         ]);
     }
 
